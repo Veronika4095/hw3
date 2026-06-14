0627a6a7a40d28861267f66851364f84d0762548
Veronika4095 <vepaklopova@edu.hse.ru>
2026-06-14 09:48:07 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,3 +3,7 @@
 build_generator() {docker build -t generator -f Dockerfile.generator .}
 
 run_generator() {mkdir -p data docker run --rm -v $(pwd)/data:/data generator}
+
+build_reporter() {docker build -t reporter -f Dockerfile.reporter}
+
+run_reporter() { docker run --rm -v $(pwd)/data:/data reporter}
